80855b27f5a8c44f95d63ce706900c91aab77f1c
Jusung Park <imjusung51@gmail.com>
2024-06-03 04:20:54 -0400

    added cmake, json and curl features to fetch player rating and RD from chess.com API

---
+++ build/CMakeFiles/ChessRating.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessRating.dir/main.cpp.o"
+  "CMakeFiles/ChessRating.dir/main.cpp.o.d"
+  "ChessRating"
+  "ChessRating.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessRating.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
